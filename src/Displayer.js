a6e152fac91c65b64ea1e63e13be30573e0c2984
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-08-22 20:17:50 -0400

    initial commit

---
+++ src/Displayer.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react';
+
+class Displayer extends Component {
+    render() {
+        const { number } = this.props;
+        return(
+            <div>{ number }</div>
+        );
+    }
+}
+export default Displayer
